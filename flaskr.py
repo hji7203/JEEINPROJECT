7b29afbfe676bed486456cda6aeb8e42d4ae5283
Hyun Jeein <hji7203@gmail.com>
2014-07-19 20:30:45 +0900

    step3

--- flaskr.py
+++ flaskr.py
@@ -24,3 +24,16 @@ def connect_db() :
 
 if __name__ == '__main__':
 	app.run()
+
+def get_db():
+	"""Opens a new database connection if there is none yet for the current application context.
+	"""
+	if not hasattr(g, 'sqlite_db'):
+		g.sqlite_db = connect_db()
+	return g.sqlite_db
+
+@app.teardown_appcontext
+def close_db(error):
+	"""Close the database again at the end of the request."""
+	if hasattr(g, 'sqlite_db'):
+		g.sqlite_db.close()
