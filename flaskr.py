77e7487716819f6a3f1bdad2b50e61f77ca48a82
Hyun Jeein <hji7203@gmail.com>
2014-07-20 15:53:56 +0900

    step4

--- flaskr.py
+++ flaskr.py
@@ -37,3 +37,17 @@ def close_db(error):
 	"""Close the database again at the end of the request."""
 	if hasattr(g, 'sqlite_db'):
 		g.sqlite_db.close()
+
+def init_db():
+	with app.app_context():
+		db = get_db()
+		with app.open_resource('schema.sql', mode ='r') as f:
+			db.cursor().executescript(f.read())
+		db.commit()
+
+from flaskr import init_db
+init_db()
+
+
+
+
